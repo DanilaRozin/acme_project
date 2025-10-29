2d277102626d3ad9e0fc3c7ab726826443b1e3e1
Danila Sunavbarov <danila.sunavbarov@mail.com>
2025-10-29 14:32:26 +0300

    Update

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
+from .forms import BirthdayForm
 
 def birthday(request):
-    context = {}
+    print(request.GET)
+    form = BirthdayForm()
+    context = {'form':form}
     return render(request, 'birthday/birthday.html', context=context)
